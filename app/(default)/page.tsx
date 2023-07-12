55ddbe342bd72f2cdabcecbb44a4e0af32b12d77
Mohammad Tanzil Idrisi <idrisim@its.beloit.edu>
2023-07-12 11:26:32 -0500

    New Changes

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <Features />
+      {/* <Features /> */}
       <FeaturesBlocks />
-      {/* <Testimonials /> */}
+       <Testimonials /> 
       <Newsletter />
     </>
   )
